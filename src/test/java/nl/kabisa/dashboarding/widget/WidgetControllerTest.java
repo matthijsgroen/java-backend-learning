aa37e4bc9d24864826d445f81a7fd213c792da7e
Matthijs Groen <matthijs.groen@gmail.com>
2026-02-02 10:44:45 +0100

    Setup initial test

---
+++ src/test/java/nl/kabisa/dashboarding/widget/WidgetControllerTest.java
@@ -0,0 +1,70 @@
+package nl.kabisa.dashboarding.widget;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.equalTo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WidgetControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void createWidget() throws Exception {
+        final String WIDGET_JSON = """
+                {
+                    "widgetClass": "google-calendar-widget",
+                    "configuration": {
+                        "title": "Lunch & Learn binnenkort",
+                        "secretIcalUrl": "https://.....",
+                        "lookAhead": 60,
+                        "lookBack": 0,
+                    },
+                    "configurationModel": [{
+                        "id": "title",
+                        "type": "string",
+                        "scope": "frontend"
+                    }, {
+                        "id": "secretIcalUrl",
+                        "type": "string",
+                        "scope": "backend",
+                        "storage": "encrypted"
+                    }, {
+                        "id": "lookAhead",
+                        "type": "integer",
+                        "scope": "frontend"
+                    }, {
+                        "id": "lookAhead",
+                        "type": "integer",
+                        "scope": "backend"
+                    }],
+                    "dataEndpoints": [{
+                        "path": "calendar",
+                        "cache": 10*60*1000,
+                        "steps": [{
+                            "action": "tunnelRequest",
+                            "config": {
+                                "method": "GET",
+                                "url": "%secretIcalUrl%",
+                            }
+                        }]
+                    }]
+                }
+                """;
+        mvc.perform(post("/widget").contentType(MediaType.APPLICATION_JSON).content(WIDGET_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().string(equalTo("{\"id\":1,\"content\":\"Hello, World!\"}")));
+    }
+
+}
